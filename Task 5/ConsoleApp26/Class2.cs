7a295a90f52cc2f4768eae58d9757c3da7b9608c
ripandtear21 <norman228337@gmail.com>
2020-09-17 21:31:42 +0300

    task

---
+++ Task 5/ConsoleApp26/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp26
+{
+    class User
+    {
+        public string login;
+        public string name;
+        public string surname;
+        public int age;
+
+        DateTime thisDay = DateTime.Now;
+        public User(string l, string n, string s, int a)
+        {
+            login = l;
+            name = n;
+            surname = s;
+            age = a;
+
+
+        }
+
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}\nФамилия: {surname}\nВозраст: {age}\nЛогин: {login} ");
+            Console.WriteLine("Дата:" + thisDay.ToString());
+        }
+    }
+}
--- Task 5/ConsoleApp26/Program.cs
+++ Task 5/ConsoleApp26/Program.cs
@@ -4,35 +4,8 @@ namespace ConsoleApp26
 {
     class Program
     {
-        class User
-        {
-            public string login;
-            public string name;
-            public string surname;
-            public int age;
-            
-            DateTime thisDay = DateTime.Now;
-            public User(string l, string n, string s, int a)
-            {
-                login = l;
-                name = n;
-                surname = s;
-                age = a;
-                
-
-            }
-
-
-
-            public void GetInfo()
-            {
-                Console.WriteLine($"Имя: {name}\nФамилия: {surname}\nВозраст: {age}\nЛогин: {login} ");
-                Console.WriteLine("Дата:"+thisDay.ToString());
-            }
-        }
         
-        
-        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Введите логин:");
@@ -45,9 +18,11 @@ namespace ConsoleApp26
             int j = Int32.Parse(Console.ReadLine());
 
 
-            User username = new User(q,z,b,j);
+            User username = new User(q, z, b, j);
             Console.Clear();
             username.GetInfo();
         }
+
+
     }
 }

753cd71c6902d8e41ac92633ba8fd316132cc3cb
ripandtear21 <norman228337@gmail.com>
2020-09-17 18:04:16 +0300

    task

---
+++ Task 2/ConsoleApp23/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        
+        class Rectangle
+        {
+            public double side1;
+            public double side2;
+            double area;
+            double perimeter;
+            public Rectangle(double s1, double s2)
+            {
+                side1 = s1;
+                side2 = s2;
+
+            }
+            
+              public double AreaCalculator()
+            {
+                area = side1 * side2;
+                return Area;
+            }
+            public double PerimeterCalculator()
+            {
+                perimeter = (2 * side1) + (2 * side2);
+                return Perimeter;
+            }
+            public double Area
+            {
+                get
+                {
+                    return area;
+                }
+            }
+            public double Perimeter
+            {
+                get
+                {
+                    return perimeter;
+                }
+            }
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону A");
+            double g = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону B");
+            double v = Convert.ToDouble(Console.ReadLine());
+            
+
+            Rectangle solution = new Rectangle(g, v);
+
+           
+            Console.WriteLine("Значение площади:"+solution.AreaCalculator());
+            Console.WriteLine("Значение периметра:"+solution.PerimeterCalculator());
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- Task 2/ConsoleApp23/obj/Debug/netcoreapp3.1/ConsoleApp23.AssemblyInfo.cs
+++ Task 2/ConsoleApp23/obj/Debug/netcoreapp3.1/ConsoleApp23.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp35")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp35")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp35")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp23")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
